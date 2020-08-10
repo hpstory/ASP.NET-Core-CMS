c259953013b6ea90d66bedf5396ae3726ecf174a
hanpeng <hanpeng@aifound.cn>
2020-08-10 17:42:45 +0800

    init

---
+++ WebApplication/Models/Comments/CommentsDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models.Comments
+{
+    public class CommentsDto
+    {
+    }
+}
